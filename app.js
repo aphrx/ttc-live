fc14a08892ff5bdfeb4dceb8011666dfab70676e
Aphrx <amalnnath20@yahoo.ca>
2021-03-02 17:36:03 -0500

    partials

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const port = 3000
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
